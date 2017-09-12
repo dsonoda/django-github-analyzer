090d93936e1a561db770409ef43694221dacafec
dsonoda <dksonoda+git@gmail.com>
2017-09-12 21:25:44 +0000

    cron実行設定追加

--- django_github_analyzer/management/commands/git_clone.sh
+++ django_github_analyzer/management/commands/git_clone.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+# mysite
 export DJANGO_SETTINGS_MODULE=mysite.settings
-. /vagrant/venv/bin/activate
-cd /path/to/src
-python manage.py some_command
+# GITHUB APPLICATION SETTING
+export GITHUB_OAUTH_CLIENT_ID=
+export GITHUB_OAUTH_CLIENT_SECRET=
+# SOURCE CODE PATH
+export GITHUB_SRC_PATH=/vagrant/github_codes/
+
+cmd="cd /vagrant"
+eval ${cmd}
+
+script1="source venv/bin/activate"
+eval ${script1}
+
+script2="python ./manage.py git_clone"
+eval ${script2}
